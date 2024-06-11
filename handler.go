56223555e19124190b8cb9f2c9c87f5fd26c4912
sapergus <94682748+sapergus@users.noreply.github.com>
2024-06-11 16:42:06 +0200

    Update handler.go

--- handler.go
+++ handler.go
@@ -1,6 +1,9 @@
 package hmac
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/caddyserver/caddy/v2"
@@ -13,17 +16,15 @@ func (m HMAC) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.H
 		// nothing to do
 		return next.ServeHTTP(w, r)
 	}
-	_, remainingPath, query, err := extractHMACAndPath(r)
+	body, err := copyRequestBody(r)
 	if err != nil {
 		return err
 	}
 
 	repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
 
-	toSign := remainingPath + query
-
 	secret := repl.ReplaceAll(m.Secret, "")
-	signature := generateSignature(m.hasher, secret, []byte(toSign))
+	signature := generateSignature(m.hasher, secret, body)
 	if err != nil {
 		return err
 	}
@@ -32,3 +33,19 @@ func (m HMAC) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.H
 	return next.ServeHTTP(w, r)
 }
 
+// copyRequestBody copies the request body while making it reusable.
+// It returns the copied []byte.
+func copyRequestBody(r *http.Request) ([]byte, error) {
+	bodyCopy := bytes.Buffer{}
+	tee := io.TeeReader(r.Body, &bodyCopy)
+	body, err := ioutil.ReadAll(tee)
+	if err != nil {
+		return nil, err
+	}
+
+	// replace the body
+	r.Body = ioutil.NopCloser(bytes.NewReader(body))
+
+	// return the copy
+	return bodyCopy.Bytes(), nil
+}
